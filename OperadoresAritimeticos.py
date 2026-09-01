5d75b9b4df119f7bd519cb95ea80311e50c0b8df
joarez_curso_python <joarezvrodrigues@gmail.com>
2026-09-01 19:58:23 -0300

    Ajuste do codigo Operadores Aritimeticos

---
+++ OperadoresAritimeticos.py
@@ -0,0 +1,28 @@
+#OPERADORES MATEMÁTICOS
+
+numero1 = 40
+numero2 = 15
+
+
+#SOMA
+
+soma = numero1 + numero2
+
+#SUBTRACAO
+
+subtracao  = numero1 - numero2
+
+#MULTIPLICACAO
+
+multiplicacao = numero1 * numero2
+
+#DIVISAO
+
+divisao = numero1 / numero2
+resto = numero1 % numero2
+
+print("A soma dos números é: ",soma)
+print("A subtração dos números é: ",subtracao)
+print("A multiplicação dos números é: ",multiplicacao)
+print("A divisão dos números é: ",divisao)
+print("O resto da divisão é: ",resto)
