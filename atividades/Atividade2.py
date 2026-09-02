cfe48a46994ef55cf1aca3553a9aac95d4d6932c
joarez_curso_python <joarezvrodrigues@gmail.com>
2026-09-01 21:49:17 -0300

    Resposta da Atividade2.py

--- atividades/Atividade2.py
+++ atividades/Atividade2.py
@@ -7,6 +7,3 @@ plano = bool(input("Você tem plano de saude: "))
 dentro = (idade > 17 or idade < 55)
 
 print("Ola {}, você tem {} anos, tem plano de saúde? {}. Você foi aceito? {}".format(nome,idade,plano,dentro))
-
-print(plano)
-print(dentro)
