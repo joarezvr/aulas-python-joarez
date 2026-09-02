4521a01ef719f6bed4d6fee61d0e59c276f53118
joarez_curso_python <joarezvrodrigues@gmail.com>
2026-09-01 21:48:36 -0300

    Resposta da Atividade2.py

--- atividades/Atividade2.py
+++ atividades/Atividade2.py
@@ -1,1 +1,12 @@
 #resposta
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+plano = bool(input("Você tem plano de saude: "))
+
+dentro = (idade > 17 or idade < 55)
+
+print("Ola {}, você tem {} anos, tem plano de saúde? {}. Você foi aceito? {}".format(nome,idade,plano,dentro))
+
+print(plano)
+print(dentro)
--- aulas/OperadoresLogicos.py
+++ aulas/OperadoresLogicos.py
@@ -1,7 +1,2 @@
 from turtledemo import planet_and_moon
 
-nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
-plano_saude = bool(input("Você tem plano de saude: "))
-
-print("Ola {}, você é um tem {} anos e "
