b08ca45c8e95ffb44a70d88eb09e4bbc38e771ba
joarez_curso_python <joarezvrodrigues@gmail.com>
2026-09-09 21:24:50 -0300

    6465

--- aulas/EstruturasDeRepeticao.py
+++ aulas/EstruturasDeRepeticao.py
@@ -1,32 +1,41 @@
-#REPETIÇÃO WHILE -> ENQUANTO
-from bdb import Breakpoint
-
-# ano_nascimento = 2004
-# ano_final = 2077
+# #REPETIÇÃO WHILE -> ENQUANTO
+# from bdb import Breakpoint
 #
-# idade = 0
-# #for i = 0, i <= 10 , i += 1
+# # ano_nascimento = 2004
+# # ano_final = 2077
+# #
+# # idade = 0
+# # #for i = 0, i <= 10 , i += 1
+# #
+# # while ano_nascimento <= ano_final:
+# #     idade += 1
+# #     print(idade)
+# #
+# #     ano_nascimento += 1
 #
-# while ano_nascimento <= ano_final:
-#     idade += 1
-#     print(idade)
 #
-#     ano_nascimento += 1
+# # nome = str(input("Digite o seu nome: "))
+# #
+# # while nome != "Joarez":
+# #     print("Pessoa inesistente")
+# #     nome = str(input("Digite novamente o seu nome: "))
+# # print("Nome encontrado")
+# tentativa = 3
+#
+# while True:
+#     opcao = input("digite uma opção: ")
+#     if opcao == "a":
+#         print("Você ganhou!")
+#             break
+#     elif tentativa == 0:
+#         print("Você perdeu!")
+#             tentativa -= 1
 
 
-# nome = str(input("Digite o seu nome: "))
-#
-# while nome != "Joarez":
-#     print("Pessoa inesistente")
-#     nome = str(input("Digite novamente o seu nome: "))
-# print("Nome encontrado")
-tentativa = 3
+#REPETIÇÃO FOR -> PARA
 
-while True:
-    opcao = input("digite uma opção: ")
-    if opcao == "a":
-        print("Você ganhou!")
-            break
-    elif tentativa == 0:
-        print("Você perdeu!")
-            tentativa -= 1
+for numero_secreto in range(1, 11):
+    print("voce ganhou")
+
+
+
---
+++ aulas/EstruturasDeRepeticaoFor.py
@@ -0,0 +1,8 @@
+#REPETIÇÃO FOR -> PARA
+#criando uma lista
+
+
+for numero_secreto in range(0, 15):
+    print(numero_secreto)
+
+
